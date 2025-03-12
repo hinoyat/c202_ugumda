1dc77f64c70c944d790a8cf3bbe6b9d3bb0b86c3
주찬 <p990805@naver.com>
2025-03-12 17:56:25 +0900

    [FE] Design: 꿈해몽 페이지 구현, 검색바 모달 살짝 디자인

---
+++ FE/src/domains/dreamSolve/components/DreamSolveContent.tsx
@@ -0,0 +1,17 @@
+const DreamSolveContent = () => {
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-evenly">
+      <div>
+        <h1 className="text-2xl font-bold text-[#6C4D2C]">🔮꿈 해몽</h1>
+      </div>
+      <div className="w-full h-[60%] flex items-center justify-center">
+        <button className="px-4 py-1 bg-[#6C4D2C] w-40 h-10 rounded text-white font-bold cursor-pointer hover:bg-amber-900 mb-20">
+          {' '}
+          분석하기{' '}
+        </button>
+      </div>
+    </div>
+  );
+};
+export default DreamSolveContent;
+// 🔮꿈 해몽
