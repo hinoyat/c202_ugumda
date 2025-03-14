4ac0917a66b987dd998338b99193104a18fd8378
주찬 <p990805@naver.com>
2025-03-14 17:18:53 +0900

    [FE] Design: 일기 조회 디자인 중

--- FE/src/domains/guestbook/GuestBook.tsx
+++ FE/src/domains/guestbook/GuestBook.tsx
@@ -1,48 +1,66 @@
-import GuestBookList from "./components/GuestBookList";
+import GuestBookList from './components/GuestBookList';
 import { IoClose } from 'react-icons/io5';
-import { useNavigate } from "react-router-dom";
-import MainPage from "../mainpage/pages/MainPage";
+import { useNavigate } from 'react-router-dom';
+import MainPage from '../mainpage/pages/MainPage';
 
 const GuestBook = () => {
-    const owner ={user_id: 1, nickname:"어린왕자", introduction:"안녕하세요 한줄 소개글 테스트 중입니다.", profile:'exampleprofile.svg' };
-    const nav = useNavigate();
-    const onClickHome =() => {
-        nav('/');
-    }
+  const owner = {
+    user_id: 1,
+    nickname: '어린왕자',
+    introduction: '안녕하세요 한줄 소개글 테스트 중입니다.',
+    profile: 'exampleprofile.svg',
+  };
+  const nav = useNavigate();
+  const onClickHome = () => {
+    nav('/');
+  };
   return (
     <div className="w-screen h-screen relative">
-        <MainPage/>
-        <div className="absolute inset-0 blur-2xl opacity-80"></div>
-        <div className="absolute w-[80%] h-[85%] bg-[#6E6E6E] opacity-80 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
-        
-        <div className="absolute w-[80%] h-[85%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <MainPage />
+      <div className="absolute inset-0 backdrop-blur-sm"></div>
+      <div className="absolute w-[80%] h-[85%] bg-[#6E6E6E] opacity-80 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+
+      <div className="absolute w-[80%] h-[85%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div
-                className="absolute z-40 top-[1%] right-[0.5%] cursor-pointer"
-                onClick={onClickHome}>
-                <IoClose className="text-white text-4xl hover:text-gray-400" />
+          className="absolute z-40 top-[1%] right-[0.5%] cursor-pointer"
+          onClick={onClickHome}>
+          <IoClose className="text-white text-4xl hover:text-gray-400" />
         </div>
-            <div className="flex flex-col p-10 gap-10 w-full">
-                {/*헤더 부분 */}
-                <div className="flex items-center justify-between">
-                    <div className="flex flex-col gap-1">
-                        <h1 className="text-white font-semibold text-2xl">{owner.nickname} 님의 방명록</h1>
-                        <p className="ml-1 text-white font-light text-sm">{owner.introduction}</p>
-                    </div>
-                    <img src={owner.profile} alt="" className="w-15"/>
-                </div>
-
-                {/*방명록 남기는 부분 */}
-                <div className="flex gap-7  justify-center">
-                    <input type="text" placeholder="내용을 입력해 주세요." className="w-[75%] p-1 border-b border-gray-300 text-gray-300 outline-none"/>
-                    <button className="bg-neutral-700 px-2 py-1 text-white rounded text-[13px] w-[10%] cursor-pointer hover:bg-neutral-400">남기기</button>
-                </div>
-
-                <GuestBookList/>
-
+        <div className="flex flex-col p-10 gap-10 w-full">
+          {/*헤더 부분 */}
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+              <h1 className="text-white font-semibold text-2xl">
+                {owner.nickname} 님의 방명록
+              </h1>
+              <p className="ml-1 text-white font-light text-sm">
+                {owner.introduction}
+              </p>
             </div>
-        </div>
-    </div>
-  )
-}
+            <img
+              src={owner.profile}
+              alt=""
+              className="w-15"
+            />
+          </div>
 
-export default GuestBook
+          {/*방명록 남기는 부분 */}
+          <div className="flex gap-7  justify-center">
+            <input
+              type="text"
+              placeholder="내용을 입력해 주세요."
+              className="w-[75%] p-1 border-b border-gray-300 text-gray-300 outline-none"
+            />
+            <button className="bg-neutral-700 px-2 py-1 text-white rounded text-[13px] w-[10%] cursor-pointer hover:bg-neutral-400">
+              남기기
+            </button>
+          </div>
+
+          <GuestBookList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GuestBook;
