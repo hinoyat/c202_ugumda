260daea14a3a7eb6d64c216a50e5e107aad09496
aodlfendb <heehyuon@gmail.com>
2025-03-14 17:05:06 +0900

    [FE] Style: 컴포넌트 스타일 수정

--- FE/src/domains/mainpage/components/UserSpaceHeader .tsx
+++ FE/src/domains/mainpage/components/UserSpaceHeader .tsx
@@ -13,13 +13,13 @@ const UserSpaceHeader = ({
 }: UserSpaceHeaderProps) => {
   return (
     <div className="p-4">
-      <p className="text-base  text-white/90">
-        <span className="font-bold">{nickname}</span> 님의 우주
-      </p>
+      <div className="flex items-baseline">
+        <h3 className="text-base font-bold text-white/90">{nickname}</h3>
+        <p className="text-sm text-white/90 ml-1">님의 우주</p>
+      </div>
       <button
         onClick={onButtonClick}
         className="mt-2 w-[80px] h-[25px] bg-white/50 text-white rounded-[6px] text-xs cursor-pointer hover:bg-white/70 transition-colors">
-        {' '}
         {buttonLabel}
       </button>
     </div>
