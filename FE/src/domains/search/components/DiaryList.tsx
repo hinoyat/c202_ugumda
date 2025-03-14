a10caf87cbc18264718747d4e3bba52118aa8d1d
주찬 <p990805@naver.com>
2025-03-14 11:50:14 +0900

    [FE] Design: 검색 목데이터로 디자인

--- FE/src/domains/search/components/DiaryList.tsx
+++ FE/src/domains/search/components/DiaryList.tsx
@@ -13,14 +13,48 @@ interface DiaryListProps {
 
 const DiaryList: React.FC<DiaryListProps> = ({ data }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-5 mt-4">
       {data.map((diary) => (
-        <div key={diary.id}>
-          <h2>{diary.title}</h2>
-          <p className="truncate">{diary.content}</p>
-          <img src={diary.profile} alt=""  className="w-6 h-6"/>
-          <p>{diary.user}</p>
-          <p>{diary.tags.join(' ')}</p>
+        <div
+          key={diary.id}
+          className="truncate w-full">
+          <div className="bg-[#6E6E6E]/47 flex px-10 py-5 gap-8 w-full">
+            {/*방명록 왼쪽 시작 */}
+            <div className="flex flex-col truncate gap-2 basis-6/8">
+              <h1 className="text-2xl text-white font-semibold">
+                {diary.title}
+              </h1>
+              <div className="px-2 flex flex-col gap-2">
+                <p className="truncate text-white w-150">{diary.content}</p>
+                <div className="flex gap-2">
+                  {diary.tags.map((tag, index) => (
+                    <p
+                      key={index}
+                      className="bg-[#D9D9D9]/29 text-white rounded text-[13px] px-2 py-1">
+                      {tag}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+            {/* 방명록 왼쪽 끝 */}
+            <div className="flex gap-5 items-center ">
+              <div className="flex gap-3 items-center">
+                <img
+                  src={diary.profile}
+                  alt=""
+                  className="w-8"
+                />
+                <p className="text-white">{diary.user}</p>
+              </div>
+
+              <div>
+                <button className="bg-[#363736] text-white px-4 py-1 rounded cursor-pointer hover:bg-neutral-500">
+                  보러가기
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
--- FE/src/domains/search/components/DiarySearch.tsx
+++ FE/src/domains/search/components/DiarySearch.tsx
@@ -1,4 +1,4 @@
-import '../styles/DiarySearch.css'
+import '../styles/DiarySearch.css';
 import DiaryList from './DiaryList';
 
 const DiarySearch = () => {
@@ -6,7 +6,8 @@ const DiarySearch = () => {
     {
       id: 1,
       title: '첫 번째 일기',
-      content: '오늘은 날씨가 참 좋았다...ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+      content:
+        '오늘은 날씨가 참 좋았다...ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
       profile: 'exampleprofile.svg',
       user: '닉네임1',
       tags: ['#태그1', '#태그2'],
@@ -22,25 +23,35 @@ const DiarySearch = () => {
     {
       id: 3,
       title: '세 번째 일기',
-      content: '내용이 조금 길어질 수 있다...ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ',
+      content:
+        '내용이 조금 길어질 수 있다...ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇssssssss',
       profile: 'exampleprofile.svg',
       user: '닉네임3',
-      tags: ['#태그3', '#태그4','태그5'],
+      tags: ['#태그3', '#태그4', '태그5'],
     },
-
   ];
 
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col gap-4">
       <div className="flex gap-5 w-full justify-center">
-        <input type="text" placeholder="내용을 입력해 주세요" className="border-b border-white p-1 basis-63/100  placeholder:text-[#FFFFFF]/70"/>
-        <button className="bg-[#545454] text-white rounded px-3 w-20">검색 </button>
+        <input
+          type="text"
+          placeholder="내용을 입력해 주세요"
+          className="border-b border-white p-1 basis-63/100  placeholder:text-[#FFFFFF]/70"
+        />
+        <button className="bg-[#545454] text-white rounded px-3 w-20">
+          검색{' '}
+        </button>
       </div>
       {/*체크 박스 시작 */}
       <div className="flex gap-4 w-full justify-start pl-30">
         <div className="flex gap-1.5 items-center">
           <label className="cont">
-            <input type="checkbox"  value="현재 우주" />
+            <input
+              type="checkbox"
+              defaultChecked
+              value="현재 우주"
+            />
             <span></span>
           </label>
           <p className="text-white">현재 우주</p>
@@ -48,7 +59,11 @@ const DiarySearch = () => {
 
         <div className="flex gap-1.5 items-center">
           <label className="cont">
-            <input type="checkbox" value="제목" />
+            <input
+              type="checkbox"
+              defaultChecked
+              value="제목"
+            />
             <span></span>
           </label>
           <p className="text-white">제목</p>
@@ -56,7 +71,11 @@ const DiarySearch = () => {
 
         <div className="flex gap-1.5 items-center">
           <label className="cont">
-            <input type="checkbox" value="내용" />
+            <input
+              type="checkbox"
+              defaultChecked
+              value="내용"
+            />
             <span></span>
           </label>
           <p className="text-white">내용</p>
@@ -64,17 +83,22 @@ const DiarySearch = () => {
 
         <div className="flex gap-1.5 items-center">
           <label className="cont">
-            <input type="checkbox" value="태그" />
+            <input
+              type="checkbox"
+              defaultChecked
+              value="태그"
+            />
             <span></span>
           </label>
           <p className="text-white">태그</p>
         </div>
       </div>
-       {/*체크 박스 끝 */}
-      <DiaryList data={sampleData}/>
-
+      {/*체크 박스 끝 */}
+      <div className="w-full h-[410px] overflow-y-auto pr-4 custom-scrollbar">
+        <DiaryList data={sampleData} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DiarySearch
+export default DiarySearch;
