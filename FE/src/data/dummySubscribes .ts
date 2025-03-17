ec69e7f163895775771116d8f44dd01519adc497
aodlfendb <heehyuon@gmail.com>
2025-03-17 21:24:55 +0900

    [FE] Test: 더미데이터 추가

---
+++ FE/src/data/dummySubscribes .ts
@@ -0,0 +1,104 @@
+// 구독 더미데이터
+
+export const dummySubscribes = [
+  {
+    subscribe_seq: 1,
+    subscribed_seq: 2,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 2,
+    subscribed_seq: 3,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 3,
+    subscribed_seq: 4,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 4,
+    subscribed_seq: 5,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 5,
+    subscribed_seq: 6,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 6,
+    subscribed_seq: 7,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 7,
+    subscribed_seq: 8,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 8,
+    subscribed_seq: 9,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 9,
+    subscribed_seq: 10,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 10,
+    subscribed_seq: 11,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 11,
+    subscribed_seq: 12,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 12,
+    subscribed_seq: 13,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 13,
+    subscribed_seq: 14,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 14,
+    subscribed_seq: 15,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 15,
+    subscribed_seq: 16,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 16,
+    subscribed_seq: 17,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 17,
+    subscribed_seq: 18,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 18,
+    subscribed_seq: 19,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 19,
+    subscribed_seq: 20,
+    created_at: '20250302122500',
+  },
+  {
+    subscribe_seq: 20,
+    subscribed_seq: 1,
+    created_at: '20250302122500',
+  },
+];
