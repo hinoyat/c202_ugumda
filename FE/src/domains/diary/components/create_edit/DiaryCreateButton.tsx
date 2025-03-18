b435cc8faff42ee747954e766865b204b7a46f61
aodlfendb <heehyuon@gmail.com>
2025-03-18 16:56:28 +0900

    [FE] Comment: 주석추가

--- FE/src/domains/diary/components/create_edit/DiaryCreateButton.tsx
+++ FE/src/domains/diary/components/create_edit/DiaryCreateButton.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+// 일기 작성 버튼 컴포넌트
+
+import React from 'react';
 
 interface DiaryCreateButtonProps {
   onCreate: () => void;
@@ -11,35 +13,32 @@ const DiaryCreateButton: React.FC<DiaryCreateButtonProps> = ({
   onCreate,
   onCreateVideo,
   Count,
-  isEditing = false
+  isEditing = false,
 }) => {
   return (
     <div className="w-full flex flex-col gap-3">
       {isEditing ? (
-        <button 
-          onClick={onCreate} 
-          className="text-[#3A3A3A] cursor-pointer w-full bg-[rgba(255,255,255,0.7)] py-2 rounded text-lg font-bold hover:bg-neutral-500 hover:text-white"
-        >
+        <button
+          onClick={onCreate}
+          className="text-[#3A3A3A] cursor-pointer w-full bg-[rgba(255,255,255,0.7)] py-2 rounded text-lg font-bold hover:bg-neutral-500 hover:text-white">
           수정완료
         </button>
       ) : (
         <>
-          <button 
-            onClick={onCreate} 
-            className="text-white cursor-pointer w-full bg-[rgba(84,84,84,1)] py-2 rounded text-lg font-bold"
-          >
+          <button
+            onClick={onCreate}
+            className="text-white cursor-pointer w-full bg-[rgba(84,84,84,1)] py-2 rounded text-lg font-bold">
             꿈 일기 등록하기
           </button>
-          <button 
-            onClick={onCreateVideo} 
-            className="text-white cursor-pointer w-full bg-[rgba(189,189,189,0.7)] py-2 rounded text-lg font-bold"
-          >
+          <button
+            onClick={onCreateVideo}
+            className="text-white cursor-pointer w-full bg-[rgba(189,189,189,0.7)] py-2 rounded text-lg font-bold">
             등록 후 영상 생성하기({Count}/3)
           </button>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default DiaryCreateButton
+export default DiaryCreateButton;
