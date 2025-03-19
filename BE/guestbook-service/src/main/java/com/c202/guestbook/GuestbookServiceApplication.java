945d560c6a7a373e30da79064da2722188d12ae2
정현수 <conjugate0519@gmail.com>
2025-03-19 11:37:37 +0900

    [BE] Feat: eureka 등록

--- BE/guestbook-service/src/main/java/com/c202/guestbook/GuestbookServiceApplication.java
+++ BE/guestbook-service/src/main/java/com/c202/guestbook/GuestbookServiceApplication.java
@@ -2,10 +2,12 @@ package com.c202.guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.c202"})
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.c202.*"})
 public class GuestbookServiceApplication {
 
 	public static void main(String[] args) {
