e004b9c272f640c1776d7768ba7258b575423b1a
aodlfendb <heehyuon@gmail.com>
2025-03-20 12:10:04 +0900

    [FE] Style: 일기 조회 모달 수정

--- FE/src/domains/diary/modals/DiaryDetail.tsx
+++ FE/src/domains/diary/modals/DiaryDetail.tsx
@@ -73,7 +73,6 @@ const DiaryDetail: React.FC<DiaryDetailProps> = ({ initialDiary, onClose }) => {
     <div className="relative w-[full] h-full">
       {isEditing ? (
         <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 transform w-[27%] h-[75%] modal-back-color p-1 z-50">
-          {' '}
           <DiaryComponent
             onClose={handleCloseEdit}
             isEditing={true}
