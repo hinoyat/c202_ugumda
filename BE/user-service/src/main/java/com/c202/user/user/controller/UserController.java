c6dc55f0817ce5c0bff15a1b2381bec33d519a89
hinoyat <hhwj2270@gmail.com>
2025-03-21 17:45:14 +0900

    [BE] Feat: 회원 자기소개 업데이트 API 추가

--- BE/user-service/src/main/java/com/c202/user/user/controller/UserController.java
+++ BE/user-service/src/main/java/com/c202/user/user/controller/UserController.java
@@ -1,43 +1,56 @@
 package com.c202.user.user.controller;
 
+import com.c202.dto.ResponseDto;
+import com.c202.user.user.model.request.UpdateIntroductionDto;
 import com.c202.user.user.model.request.UpdateUserRequestDto;
 import com.c202.user.user.model.response.UserResponseDto;
 import com.c202.user.user.service.UserService;
-import com.c202.user.global.common.response.ApiResponse;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Validated
 public class UserController {
 
     private final UserService userService;
 
     // 사용자 정보 조회
     @GetMapping("/me")
-    public ResponseEntity<ApiResponse<UserResponseDto>> getUserInfo(@RequestHeader("X-User-Seq") int userSeq) {
+    public ResponseEntity<ResponseDto<UserResponseDto>> getUserInfo(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq) {
         UserResponseDto user = userService.getUserInfo(userSeq);
-        return ResponseEntity.ok(ApiResponse.success(user, "사용자 정보 조회 성공"));
+        return ResponseEntity.ok(ResponseDto.success(200, "사용자 정보 조회 성공", user));
     }
 
     // 사용자 정보 수정
     @PutMapping("/me")
-    public ResponseEntity<ApiResponse<UserResponseDto>> updateUser(
-            @RequestHeader("X-User-Seq") int userSeq,
+    public ResponseEntity<ResponseDto<UserResponseDto>> updateUser(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq,
             @RequestBody UpdateUserRequestDto requestDto) {
-        UserResponseDto user = userService.updateUser(userSeq, requestDto);
-        return ResponseEntity.ok(ApiResponse.success(user, "사용자 정보 수정 성공"));
+        UserResponseDto updatedUser = userService.updateUser(userSeq, requestDto);
+        return ResponseEntity.ok(ResponseDto.success(200, "사용자 정보 수정 성공", updatedUser));
     }
 
     // 회원 탈퇴
     @DeleteMapping("/me")
-    public ResponseEntity<ApiResponse<Void>> deleteUser(@RequestHeader("X-User-Seq") int userSeq) {
+    public ResponseEntity<ResponseDto<Void>> deleteUser(
+            @RequestHeader("X-User-Seq")  @NotNull Integer userSeq) {
         userService.deleteUser(userSeq);
-        return ResponseEntity.ok(ApiResponse.success(null, "회원 탈퇴 성공"));
+        return ResponseEntity.ok(ResponseDto.success(204, "회원 탈퇴 성공"));
+    }
+
+    @PutMapping("/me/intro")
+    public ResponseEntity<ResponseDto<UserResponseDto>> updateIntroduction(
+            @RequestHeader("X-User-Seq") @NotNull Integer userSeq,
+            @RequestBody UpdateIntroductionDto requestDto) {
+        return ResponseEntity.ok(ResponseDto.success(201, "자기소개 수정 완료", userService.updateIntroduction(userSeq, requestDto)));
     }
 
 }
