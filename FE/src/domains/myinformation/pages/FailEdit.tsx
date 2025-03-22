725e5c096b8f7217698372744ce704238c4d0cc8
aodlfendb <heehyuon@gmail.com>
2025-03-23 05:08:17 +0900

    [FE] Style: 회원수정 폰트적용

--- FE/src/domains/myinformation/pages/FailEdit.tsx
+++ FE/src/domains/myinformation/pages/FailEdit.tsx
@@ -1,6 +1,6 @@
 import SpaceShipComponent from '@/domains/spaceship/components/SpaceShipComponent';
 import { useNavigate } from 'react-router-dom';
-import smallFrame from "@/assets/images/smallFrame.svg";
+import smallFrame from '@/assets/images/smallFrame.svg';
 
 const FailEdit = () => {
   const nav = useNavigate();
@@ -10,20 +10,20 @@ const FailEdit = () => {
   return (
     <SpaceShipComponent>
       <div className="absolute inset-0">
-        <div className="absolute inset-0 bg-black opacity-40 z-20"></div>
+        <div className="absolute inset-0 bg-black opacity-50 z-20"></div>
         <img
           src={smallFrame}
           alt=""
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30 w-200 animate-pulse"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30 w-[600px] animate-pulse"
         />
-        <div className="absolute flex flex-col gap-10 items-center justify-center z-50 top-7/14 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-pulse">
-          <p className="text-[#86F5FF] galumuri-font text-3xl">
+        <div className="absolute flex flex-col gap-4 items-center justify-center z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-pulse">
+          <p className="text-[#86F5FF] dung-font text-xl tracking-widest">
             회원수정이 실패했습니다.
           </p>
           <div className="box-button">
             <button
               onClick={onClickHome}
-              className="button w-35">
+              className="button w-25 dung-font">
               <span>확인</span>
             </button>
           </div>
