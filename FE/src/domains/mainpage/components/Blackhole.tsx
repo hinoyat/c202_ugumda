492c7e150eaffd57cf0be44cbbfd7a3be967051a
aodlfendb <heehyuon@gmail.com>
2025-03-24 11:17:23 +0900

    [FE] Feat: 방명록, 블랙홀 툴팁 추가

--- FE/src/domains/mainpage/components/Blackhole.tsx
+++ FE/src/domains/mainpage/components/Blackhole.tsx
@@ -1,6 +1,7 @@
 // 블랙홀( 다른 사람 페이지로 랜덤이동 ) 컴포넌트
 import React from 'react';
 import '../themes/Blackhole.css';
+import { Tooltip } from 'react-tooltip';
 
 const Blackhole = () => {
   const handleBlackholeClick = () => {
@@ -9,24 +10,33 @@ const Blackhole = () => {
   };
 
   return (
-    <div
-      className="blackhole-container cursor-pointer"
-      onClick={handleBlackholeClick}>
-      <div className="blackhole">
-        <div className="blackhole-circle"></div>
-        <div className="blackhole-disc"></div>
+    <>
+      <div
+        className="blackhole-container cursor-pointer"
+        onClick={handleBlackholeClick}
+        data-tooltip-id="blackhole-tooltip"
+        data-tooltip-content="랜덤으로 다른 유저의 우주로 이동">
+        <div className="blackhole">
+          <div className="blackhole-circle"></div>
+          <div className="blackhole-disc"></div>
+        </div>
+        <div className="curve">
+          <svg viewBox="0 0 500 500">
+            <path
+              id="loading"
+              d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"></path>
+            <text width="500">
+              <textPath xlinkHref="#loading">Interstellar Jump</textPath>
+            </text>
+          </svg>
+        </div>
       </div>
-      <div className="curve">
-        <svg viewBox="0 0 500 500">
-          <path
-            id="loading"
-            d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"></path>
-          <text width="500">
-            <textPath xlinkHref="#loading">Interstellar Jump</textPath>
-          </text>
-        </svg>
-      </div>
-    </div>
+
+      <Tooltip
+        id="blackhole-tooltip"
+        place="top"
+      />
+    </>
   );
 };
 
