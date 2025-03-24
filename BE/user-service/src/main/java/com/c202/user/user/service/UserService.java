cd4526d4808d19e59e37ab19de9417da67acb8a8
정현수 <conjugate0519@gmail.com>
2025-03-24 10:07:38 +0900

    [BE] Fix: merge conflict

--- BE/user-service/src/main/java/com/c202/user/user/service/UserService.java
+++ BE/user-service/src/main/java/com/c202/user/user/service/UserService.java
@@ -13,4 +13,6 @@ public interface UserService {
     void deleteUser(Integer userSeq);
 
     UserResponseDto updateIntroduction(Integer userSeq, UpdateIntroductionDto introduction);
+
+    String getUserBirthDate(Integer useSeq);
 }
