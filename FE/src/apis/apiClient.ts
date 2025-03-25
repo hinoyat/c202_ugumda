b5c9fc7ccb501139db4280645a804157c5b8d4d3
chaehyeon <chi_927@naver.com>
2025-03-25 16:36:12 +0900

    [FE] Feat: 요청 인터셉터에서 jwt decode 하기

--- FE/src/apis/apiClient.ts
+++ FE/src/apis/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 import { store } from '../stores/store';
-import { refreshToken, logoutUser } from '../stores/auth/authThunks';
+import { logoutUser } from '../stores/auth/authThunks';
+import { jwtDecode } from 'jwt-decode';
 
 // Axios 인스턴스 생성
 const api = axios.create({
@@ -10,30 +10,34 @@ const api = axios.create({
 
 // 요청 인터셉터 - Authorization 헤더 자동 추가
 api.interceptors.request.use((config) => {
-  const token = Cookies.get('accessToken');
+  const token: string | null = localStorage.getItem('accessToken');
+  const jwtTMP = jwtDecode(token);
+
+  console.log('인터셉터', jwtTMP);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+    config.headers['X-User-Seq'] = jwtTMP.userSeq;
   }
   return config;
 });
 
 // 응답 인터셉터 - 401 (Unauthorized) 발생 시 토큰 갱신
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401) {
-      try {
-        const refreshResult = await store.dispatch(refreshToken()).unwrap();
-        error.config.headers.Authorization = `Bearer ${refreshResult.accessToken}`;
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     if (error.response?.status === 401) {
+//       try {
+//         const refreshResult = await store.dispatch(refreshToken()).unwrap();
+//         error.config.headers.Authorization = `Bearer ${refreshResult.accessToken}`;
 
-        // 원래 요청 재시도
-        return axios(error.config);
-      } catch {
-        store.dispatch(logoutUser());
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+//         // 원래 요청 재시도
+//         return axios(error.config);
+//       } catch {
+//         store.dispatch(logoutUser());
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
