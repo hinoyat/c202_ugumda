974f6b7dc05caa1bdbad18207901e52f5561509d
chaehyeon <chi_927@naver.com>
2025-03-25 16:32:10 +0900

    [FE] Feat: 스토어 초기값 수정

--- FE/src/stores/auth/authSlice.ts
+++ FE/src/stores/auth/authSlice.ts
@@ -1,19 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import Cookies from 'js-cookie';
-import { loginUser, refreshToken, logoutUser } from './authThunks';
-import { AuthState } from './authTypes';
+import { loginUser, logoutUser } from './authThunks';
+import { AuthState, User } from './authTypes';
 
 // 로그인 상태 인터페이스
 // interface AuthState {
-//   username: string | null;
+//   user: User | null;
 //   accessToken: string | null;
 //   isAuthenticated: boolean;
 //}
 
 const initialState: AuthState = {
-  // username: null,
-  accessToken: Cookies.get('accessToken') || null, // 쿠키에서 토큰 가져옴
-  isAuthenticated: !!Cookies.get('accessToken'), // 토큰 존재 여부 확인
+  user: JSON.parse(localStorage.getItem('User') || 'null'),
+  accessToken: localStorage.getItem('accessToken') || null, // 로컬스토리지에서 토큰 가져옴
+  isAuthenticated: !!localStorage.getItem('accessToken'), // 토큰 존재 여부 확인
 };
 
 const authSlice = createSlice({
@@ -24,19 +23,20 @@ const authSlice = createSlice({
     builder
       // 로그인 성공
       .addCase(loginUser.fulfilled, (state, action) => {
-        // state.username = action.payload.username;
         state.accessToken = action.payload.accessToken;
         state.isAuthenticated = true;
+        state.user = action.payload.user;
       })
       // 토큰 갱신 성공
-      .addCase(refreshToken.fulfilled, (state, action) => {
-        state.accessToken = action.payload.accessToken;
-      })
+      // .addCase(refreshToken.fulfilled, (state, action) => {
+      //   state.accessToken = action.payload.accessToken;
+      // })
       // 로그아웃 시 상태 초기화
       .addCase(logoutUser.fulfilled, (state) => {
-        // state.username = null;
+        state.user = null;
         state.accessToken = null;
         state.isAuthenticated = false;
+        localStorage.removeItem('User');
       });
   },
 });
