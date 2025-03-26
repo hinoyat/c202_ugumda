2280822e73f10a3de48260f7e10985e48acf120f
seorinkim <seorin66@naver.com>
2025-03-26 09:23:02 +0900

    [AI] Feat: 텍스트, 이미지 비디오 모델 테스트 완료

--- AI/app/core/chat_stability_service.py
+++ AI/app/core/chat_stability_service.py
@@ -26,8 +26,14 @@ def stability(prompt:str):
     logging.info("이미지 생성완료")
 
     if response.status_code == 200:
+        logging.info(f"응답 시작 부분: {str(response.content[:30])}")
+        logging.info(f"응답 Content-Type: {response.headers.get('Content-Type')}")
         img_length = len(response.content)
         logging.info(f"이미지64 길이:{img_length}")
-        return response.content
+
+        response_json = response.json()
+        base64_image = response_json["image"]
+
+        return base64_image
     else:
         raise Exception(str(response.json()))
