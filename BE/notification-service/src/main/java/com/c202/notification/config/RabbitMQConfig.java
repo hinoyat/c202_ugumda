c414abe9f886fd5dcf53ba3177fc3450ffc6342c
Hinoyat <hhwj2270@gmail.com>
2025-03-27 23:01:29 +0900

    [BE] Feat: JsonMessageConverter 추가

--- BE/notification-service/src/main/java/com/c202/notification/config/RabbitMQConfig.java
+++ BE/notification-service/src/main/java/com/c202/notification/config/RabbitMQConfig.java
@@ -6,6 +6,9 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 @Configuration
 public class RabbitMQConfig {
@@ -30,6 +33,18 @@ public class RabbitMQConfig {
                 .bind(alarmQueue())
                 .to(alarmExchange())
                 .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 
 }
