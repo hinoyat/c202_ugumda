65586157ebc588ea5d49dcb0832f313a492aeeaa
정현수 <conjugate0519@gmail.com>
2025-03-27 11:31:39 +0900

    [BE] Feat: ResponseDto에 LocalDateTime 필드 추가

--- BE/common-module/src/main/java/com/c202/dto/ResponseDto.java
+++ BE/common-module/src/main/java/com/c202/dto/ResponseDto.java
@@ -3,30 +3,28 @@ package com.c202.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class ResponseDto<T> {
-    private int status;
-    private String message;
-    private T data;
+    private final LocalDateTime timestamp;
+    private final int status;
+    private final String message;
+    private final T data;
 
-    public static <T> ResponseDto<T> success(int status, String message) {
+    public static <T> ResponseDto<T> success(T data) {
         return ResponseDto.<T>builder()
-                .status(status)
-                .message(message)
-                .build();
-    }
-
-    public static <T> ResponseDto<T> success(int status, String message, T data) {
-        return ResponseDto.<T>builder()
-                .status(status)
-                .message(message)
+                .timestamp(LocalDateTime.now())
+                .status(200)
+                .message("요청이 성공적으로 처리되었습니다.")
                 .data(data)
                 .build();
     }
 
-    public static ResponseDto<Void> error(int status, String message) {
-        return ResponseDto.<Void>builder()
+    public static <T> ResponseDto<T> error(int status, String message) {
+        return ResponseDto.<T>builder()
+                .timestamp(LocalDateTime.now())
                 .status(status)
                 .message(message)
                 .build();
