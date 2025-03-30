c7cbaf758fcba3cc9610ca53fcff280482b8e861
임채현 <chi_927@naver.com>
2025-03-30 22:39:28 +0900

    [FE] Feat: 방문 중인 메인페이지 유저정보 저장용 store

--- FE/src/stores/store.ts
+++ FE/src/stores/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import signupReducer from '@/domains/signup/stores/signupSlice';
+import userpageReducer from '@/domains/mainpage/stores/userSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer, // auth 슬라이스 등록
     signup: signupReducer,
+    userpage: userpageReducer,
   },
 });
 
