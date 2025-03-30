d393ab582588b434a2b791ab73af224c30605800
임채현 <chi_927@naver.com>
2025-03-30 22:40:01 +0900

    [FE] Feat: 라우터 수정됨에 따라 바뀌는 코드 수정

--- FE/src/domains/mainpage/pages/MainPage.tsx
+++ FE/src/domains/mainpage/pages/MainPage.tsx
@@ -5,18 +5,25 @@ import GuestbookIcon from '@/domains/mainpage/components/GuestbookIcon';
 import Universe from '@/domains/mainpage/components/universe/Universe';
 import UserSpaceHeader from '@/domains/mainpage/components/UserSpaceHeader ';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { useAppDispatch } from '@/hooks/hooks';
 import { logoutUser } from '@/stores/auth/authThunks';
 import { useSelector } from 'react-redux';
 import { selectUser } from '@/stores/auth/authSelectors';
+import { selectVisitUser } from '../stores/userSelectors';
+import { visitUserpage } from '../stores/userThunks';
 
 const MainPage = () => {
   console.log('🟡 내 메인 렌더링!');
+
   const dispatch = useAppDispatch();
-  const user = useSelector(selectUser);
+  const loginUser = useSelector(selectUser);
+  const params = useParams();
+
+  const visitUser = useSelector(selectVisitUser);
+
+  const nav = useNavigate();
 
   //      방명록 표시 여부      //
   const [showGuestbook, setShowGuestbook] = useState(false);
@@ -25,20 +32,23 @@ const MainPage = () => {
   };
 
   useEffect(() => {
-    console.log('✅ MainPage 마운트됨!');
-  }, []);
+    if (params.username) {
+      console.log('✅ MainPage 마운트됨!');
+      // dispatch(visitUserpage({ username: params.username }));
+      dispatch(visitUserpage({ username: '2' })); // 일단 백이 userSeq니까 2 해두자
+      console.log('방문한 페이지 주인장 정보', visitUser);
+    }
+  }, [params.username, dispatch]);
 
-  const nav = useNavigate();
-
-  const onClickLogin = () => {
-    nav('/login');
-  };
-  const onClickSignup = () => {
-    nav('/signup');
-  };
+  // 방문하려는 유저 정보를 가져오는 데 실패하면 내 페이지로 이동
+  useEffect(() => {
+    if (params.username === '') {
+      nav(`/${loginUser?.username}`, { replace: true });
+    }
+  }, [params.username, nav]);
 
   //          좌측 상단 UserSpaceHeader 컴포넌트          //
-  const isMySpace = true; // 내 우주인지 여부
+  const isMySpace = params.username === loginUser?.username ? true : false; // 내 우주인지 여부
   // const myNickname = '내 우주일때';
   const othernickname = '다른 유저 우주일때';
 
@@ -56,7 +66,7 @@ const MainPage = () => {
       {/* 닉네임님의 우주입니다 & 버튼 */}
       <div className="absolute top-5 left-5">
         <UserSpaceHeader
-          nickname={isMySpace ? user?.nickname : othernickname}
+          nickname={isMySpace ? loginUser?.nickname : visitUser?.nickname}
           onButtonClick={handleButtonClick}
           buttonLabel={isMySpace ? '로그아웃' : '구독취소'}
         />
