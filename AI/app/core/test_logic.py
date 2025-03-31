d24e141df5169ebfd098c9f32a0de650f1849cd2
seorinkim <seorin66@naver.com>
2025-03-31 11:15:54 +0900

    [AI] Fix: ai url을 api 추가하여 변경

--- AI/app/core/test_logic.py
+++ AI/app/core/test_logic.py
@@ -1,8 +1,11 @@
 from app.log_config import logging_check
+import os
 import logging
 import requests
 
 logging_check()
+
+API_BASE_URL = os.environ.get("API_BASE_URL")
 
 def test_logic_1(content:str, diary_pk: int, token: str):
     logging.info("시작")
@@ -34,7 +37,7 @@ def test_logic_2(diary_pk:int, video_url:str, token:str):
         logging.info(f"요청 페이로드: {payload}")
         logging.info(f"사용된 diary_pk: {diary_pk}")
 
-        response = requests.post(f'https://j12c202.p.ssafy.io/api/diaries/{diary_pk}/video', json=payload,
+        response = requests.post(f'{API_BASE_URL}/diaries/{diary_pk}/video', json=payload,
                                  headers=headers)
         logging.info(f"요청 보내는 중")
 
