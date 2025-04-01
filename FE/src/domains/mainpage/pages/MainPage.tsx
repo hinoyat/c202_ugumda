1af23e2f535f2313b01a1e0456c2c78a6c4a9141
aodlfendb <heehyuon@gmail.com>
2025-04-01 11:02:32 +0900

    [FE] Refactor: 방명록 태양 삭제

--- FE/src/domains/mainpage/pages/MainPage.tsx
+++ FE/src/domains/mainpage/pages/MainPage.tsx
@@ -62,7 +62,6 @@ const MainPage = () => {
     <div className="flex flex-col items-start text-white relative w-screen h-screen overflow-hidden">
       {/* 우주영역 */}
       <Universe isMySpace={true} />
-
       {/* 닉네임님의 우주입니다 & 버튼 */}
       <div className="absolute top-5 left-5">
         <UserSpaceHeader
@@ -71,7 +70,6 @@ const MainPage = () => {
           buttonLabel={isMySpace ? '로그아웃' : '구독취소'}
         />
       </div>
-
       {/* 주짜니 쓰는 부분  relative z-10 해서 위로 올려둠 */}
       {/* <div className="flex flex-col absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <p>메인페이지 입니다.</p>
@@ -108,14 +106,12 @@ const MainPage = () => {
           꿈해몽
         </Link>
       </div> */}
-
-      {/* 방명록 */}
+      {/* 방명록
       <div className="absolute bottom-0 left-5 mb-10 ml-5 z-10">
         <div onClick={onClickGuestBookModal}>
           <GuestbookIcon size={0.5} />
         </div>
-      </div>
-
+      </div> */}
       {/* 방명록 모달 (조건부 렌더링) */}
       {showGuestbook && <GuestBook onClose={onClickGuestBookModal} />}
     </div>
