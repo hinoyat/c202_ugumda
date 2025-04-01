e1380d310839c1e763f25c41685e95197a8bd917
hinoyat <hhwj2270@gmail.com>
2025-04-01 17:44:54 +0900

    [BE] Feat: elastic 기본 설정 & 인덱싱 추가

---
+++ BE/diary-service/src/main/java/com/c202/diary/elastic/config/ElasticSearchConfig.java
@@ -0,0 +1,19 @@
+package com.c202.diary.elastic.config;
+
+import com.c202.diary.elastic.repository.DiarySearchRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackageClasses = DiarySearchRepository.class)
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("elasticsearch:9200")
+                .build();
+    }
+}
---
+++ BE/diary-service/src/main/java/com/c202/diary/elastic/service/ElasticSearchInitService.java
@@ -0,0 +1,27 @@
+package com.c202.diary.elastic.service;
+
+import com.c202.diary.diary.entity.Diary;
+import com.c202.diary.diary.repository.DiaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ElasticSearchInitService {
+
+    private final DiaryRepository diaryRepository;
+    private final DiaryIndexService diaryIndexService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initializeElasticSearch() {
+        List<Diary> diaries = diaryRepository.findByIsDeleted("N");
+
+        for (Diary diary : diaries) {
+            diaryIndexService.indexDiary(diary);
+        }
+    }
+}
