e82a7a53711272d4902478219c124e5379c79138
aodlfendb <heehyuon@gmail.com>
2025-04-02 09:03:51 +0900

    [FE] Feat: 영상생성 기능

--- FE/src/domains/diary/components/details/DetailVideo.tsx
+++ FE/src/domains/diary/components/details/DetailVideo.tsx
@@ -3,21 +3,13 @@ interface DiaryDetailProps {
   dream_video?: string | null;
 }
 
-// const DetailVideo: React.FC<DiaryDetailProps> = ({ dream_video }) => {
-//   return (
-//     <div>
-//       <video src={dream_video}></video>
-//     </div>
-//   );
-// };
+const DetailVideo: React.FC<DiaryDetailProps> = ({ dream_video }) => {
+  const videoSrc = dream_video || miroVideo;
 
-const DetailVideo: React.FC<DiaryDetailProps> = ({
-  dream_video = miroVideo, // 기본값으로 miroVideo 설정
-}) => {
   return (
     <div className="w-full h-auto">
       <video
-        src={dream_video}
+        src={videoSrc}
         controls
         className="w-full rounded-lg shadow-md"
         playsInline>
