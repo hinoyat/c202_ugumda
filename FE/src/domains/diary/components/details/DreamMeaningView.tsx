0a42c279120b8e1efa7aae098964d51fb0dc96ae
aodlfendb <heehyuon@gmail.com>
2025-04-05 15:07:01 +0900

    [FE] Feat: 꿈해몽 뷰 컴포넌트 추가

---
+++ FE/src/domains/diary/components/details/DreamMeaningView.tsx
@@ -0,0 +1,23 @@
+// 일기조회에서의 꿈해몽 컴포넌트
+
+import { DreamSolveResponse } from '@/domains/diary/api/dreamApi';
+
+interface DreamMeaningViewProps {
+  resultContent: string;
+  isGood?: string;
+  inputContent?: string;
+}
+
+const DreamMeaningView: React.FC<DreamMeaningViewProps> = ({
+  resultContent,
+  isGood,
+  inputContent,
+}) => {
+  return (
+    <div className="text-white tracking-wide text-sm inset-0">
+      <div className="mb-2">{resultContent}</div>
+    </div>
+  );
+};
+
+export default DreamMeaningView;
