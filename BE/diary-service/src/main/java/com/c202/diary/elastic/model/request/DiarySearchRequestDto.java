43af1353907561d8849ec9d38fd146b62597b6ad
Hinoyat <hhwj2270@gmail.com>
2025-04-05 17:03:58 +0900

    [BE] Feat: 다른 유저의 우주 검색 조건 추가

--- BE/diary-service/src/main/java/com/c202/diary/elastic/model/request/DiarySearchRequestDto.java
+++ BE/diary-service/src/main/java/com/c202/diary/elastic/model/request/DiarySearchRequestDto.java
@@ -14,7 +14,9 @@ public class DiarySearchRequestDto {
     private boolean searchContent;
     private boolean searchTag;
     private boolean currentUserOnly;
+    private boolean useCurrentUser; // 지금 로그인 되어있는 사용자 userSeq 사용여부
 
+    private Integer targetUserSeq; // useCurrentUser 값이 false일 때 넣어줘야 함
     private Integer page;
     private Integer size;
 }
