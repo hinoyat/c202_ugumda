192470c6619304af99212ac4a074ae4ef155047e
seorinkim <seorin66@naver.com>
2025-04-09 09:05:56 +0900

    [FE] Refactor: dev 브랜치와 병합

--- FE/src/domains/diary/api/videoApi.ts
+++ FE/src/domains/diary/api/videoApi.ts
@@ -14,8 +14,8 @@ export const videoApi = {
 
     // return await axios.post('/ai/test-sample-video', videoData, {
     return await api.post(
-      '/ai/test-sample-video', // 요청 확인용
-      // '/ai/create-video', // 이걸로 보내야 영상 생성됨
+      // '/ai/test-sample-video', // 요청 확인용
+      '/ai/create-video', // 이걸로 보내야 영상 생성됨
       videoData
       // headers: {
 
